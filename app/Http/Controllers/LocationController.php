6b039cb1f64a55bf0374c46cccb4793885bc4846
Sam Tucker <hello@samtucker.net>
2022-09-13 22:09:17 +0100

    Add controller for Location

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Location;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function index(Request $request)
+    {
+        
+    }
+}
