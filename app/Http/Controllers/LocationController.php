31e916466c14fee40b64ba9b0d18850b44d263f9
Sam Tucker <hello@samtucker.net>
2022-09-13 22:10:22 +0100

    Validate request and assign to vars

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -15,6 +15,14 @@ class LocationController extends Controller
      */
     public function index(Request $request)
     {
-        
+        $request->validate([
+            'latitude' => ['required','regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/'],
+            'longitude' => ['required','regex:/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/'],
+            'radius' => 'required|numeric|min:0',
+        ]);
+
+        $lat = $request->latitude;
+        $lng = $request->longitude;
+        $radius = $request->radius;
     }
 }
