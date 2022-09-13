095ddf7bd2062d883508962f1ae9d0955987be6b
Sam Tucker <hello@samtucker.net>
2022-09-13 22:09:40 +0100

    Add route for api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//Route::get('/search', [LocationController::class, 'index']);
+
+Route::get('/search', [LocationController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
